53f34954d62ccd04fc744e264a219dcdec118399
Arpita Dasgupta <dasguptaarpita1999@gmail.com>
2021-09-01 05:47:18 +0530

    button translate

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 var textInput = document.querySelector("#text-input");
+var buttonTranslate = document.querySelector("#button-translate");
+buttonTranslate.addEventListener("click", buttonClickHandler)
+
+
