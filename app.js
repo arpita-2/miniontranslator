23c2ac4666289dddde3a468f35fd959d78f29912
Arpita Dasgupta <dasguptaarpita1999@gmail.com>
2021-09-01 05:50:37 +0530

    error handler

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ buttonTranslate.addEventListener("click", buttonClickHandler)
 
 var translateOutput = document.querySelector("#translate-output");
 
+var serverurl = "https://api.funtranslations.com/translate/mandalorian.json"
+
+function getTranslationURL(input){
+    return serverurl+"?"+"text= "+input 
+}
+
+function errorHandler(error)
+{console.log("Error Occured!");
+alert("Something went wrong! please try again");
+}
+
 
