fce223193d2a5e9388ec1bc92b54d94d17a873ca
Arpita Dasgupta <dasguptaarpita1999@gmail.com>
2021-09-01 05:48:19 +0530

    translate output

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ var textInput = document.querySelector("#text-input");
 var buttonTranslate = document.querySelector("#button-translate");
 buttonTranslate.addEventListener("click", buttonClickHandler)
 
+var translateOutput = document.querySelector("#translate-output");
+
 
